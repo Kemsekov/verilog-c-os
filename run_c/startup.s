f5bac7945c87e2328adf75b9570b8e46ab137e08
vlad <vlad@example.com>
2026-01-30 19:17:32 +0500

    Add xv6 running support, remove remaining xv6 stuff from run_c folder

--- run_c/startup.s
+++ run_c/startup.s
@@ -18,14 +18,53 @@ boot_vector:
 .global _start
 
 _start:
+    /* Initialize all registers to zero */
+    addi x0, x0, 0     /* x0 is always zero */
+    addi x1, x0, 0
+    addi x2, x0, 0
+    addi x3, x0, 0
+    addi x4, x0, 0
+    addi x5, x0, 0
+    addi x6, x0, 0
+    addi x7, x0, 0
+    addi x8, x0, 0
+    addi x9, x0, 0
+    addi x10, x0, 0
+    addi x11, x0, 0
+    addi x12, x0, 0
+    addi x13, x0, 0
+    addi x14, x0, 0
+    addi x15, x0, 0
+    addi x16, x0, 0
+    addi x17, x0, 0
+    addi x18, x0, 0
+    addi x19, x0, 0
+    addi x20, x0, 0
+    addi x21, x0, 0
+    addi x22, x0, 0
+    addi x23, x0, 0
+    addi x24, x0, 0
+    addi x25, x0, 0
+    addi x26, x0, 0
+    addi x27, x0, 0
+    addi x28, x0, 0
+    addi x29, x0, 0
+    addi x30, x0, 0
+    addi x31, x0, 0
+
     /* Initialize stack pointer */
-    /* Use a safe location in memory for stack */
-    lui sp, 0x10       /* Load upper part of address (0x10000) */
-    addi sp, sp, 0     /* Adjust to actual stack pointer */
+    /* Use a safe location in memory for stack - start from high memory and go down */
+    lui sp, 0x3      /* Load upper part of address (0x3000) */
+    addi sp, sp, 0   /* Point to 0x3000 as stack top (before bss section) */
 
     /* Set up trap vector to a simple handler that just returns */
     la t0, simple_trap_handler
     csrw mtvec, t0     /* Set machine trap vector */
+
+    /* Clear CSR registers that might cause issues */
+    csrw mstatus, x0
+    csrw mie, x0
+    csrw mip, x0
 
     /* Call main function */
     call main
